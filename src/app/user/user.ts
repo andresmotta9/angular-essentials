435cdfc4cd74fc3fb79f5df1bf540de8ce064431
amotta-tv <andres@turbovets.com>
2025-10-28 09:40:15 -0400

    Adding extra type to the event emitter

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -10,7 +10,7 @@ export class User {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
   // select = output<string>();
 
   // avatar = input.required<string>();
