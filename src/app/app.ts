1dfab600454374b8ad21af4c07cb2a6dabfc0d60
amotta-tv <andres@turbovets.com>
2025-10-28 08:56:07 -0400

    Initial setup of task management system

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('essentials');
-}
+export class App {}
